31d8e1cd77d9b019d585e2d2446db526f56bb568
Oliver Harrison <oliverharrison255@gmail.com>
2022-04-08 21:17:47 -0700

    make translation instead of force

--- LBDEngine/Headers/Behaviours/Pathfinding.h
+++ LBDEngine/Headers/Behaviours/Pathfinding.h
@@ -13,99 +13,14 @@ public:
 
     std::vector<Node> pathToGoal;
 
-    // Gets the grid from the MyGrid Script
-    /*void Awake()
-    {
-        grid = GetComponent<MyGrid>();
-    }*/
-
-
     // Refer to the PSEUDO-CODE in A* algorithm notes
-    void FindPath(Vector3 startPos, Vector3 targetPos)
-    {
-        Node startNode = grid.NodeFromWorldPoint(startPos);
-        Node targetNode = grid.NodeFromWorldPoint(targetPos);
-
-        // Set of nodes to be evaluated
-        std::vector<Node> openSet;
-        // Set of nodes already evaluated
-        std::vector<Node> closedSet;
-        openSet.push_back(startNode);
-
-        while (openSet.size() > 0)
-        {
-            Node currentNode = openSet[0];
-            
-            for (int i = 1; i < openSet.size(); i++)
-            {
-                if (openSet[i].fCost() < currentNode.fCost() || openSet[i].fCost() == currentNode.fCost() && openSet[i].hCost < currentNode.hCost)
-                {
-                    currentNode = openSet[i];
-                }
-            }
-
-            openSet.erase(std::remove(openSet.begin(), openSet.end(), currentNode), openSet.end());
-            closedSet.push_back(currentNode);
-
-            if (currentNode == targetNode)
-            {
-                RetracePath(startNode, currentNode);
-                return;
-            }
-
-            for (Node neighbour : grid.GetNeighbours(currentNode))
-            {
-                if (!neighbour.walkable || std::count(closedSet.begin(), closedSet.end(), neighbour))
-                {
-                    continue;
-                }
-
-                int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
-                if (newMovementCostToNeighbour < neighbour.gCost || !(std::find(openSet.begin(), openSet.end(), neighbour) != openSet.end()))
-                {
-                    neighbour.gCost = newMovementCostToNeighbour;
-                    neighbour.hCost = GetDistance(neighbour, targetNode);
-                    neighbour.parent = &currentNode;
-
-                    // This just check if it doesn't contain
-                    if (!(std::count(openSet.begin(), openSet.end(), neighbour)))
-                    {
-                        openSet.push_back(neighbour);
-                    }
-                }
-            }
-        }
-    }
+    void FindPath(Vector3 startPos, Vector3 targetPos);
 
     // Retraces and calculates what the path it took was, the parent node in the Node class is vital
-    void RetracePath(Node startNode, Node endNode)
-    {
-        std::vector<Node> path;
-        Node currentNode = endNode;
-
-        while (currentNode != startNode)
-        {
-            path.push_back(currentNode);
-            currentNode = *currentNode.parent;
-        }
-        // Path will be backwards, need to reverse
-        std::reverse(path.begin(), path.end());
-        pathToGoal = path;
-    }
+    void RetracePath(Node startNode, Node endNode);
 
     // Function to get the distance of two nodes
-    int GetDistance(Node nodeA, Node nodeB)
-    {
-        int dstX = abs(nodeA.gridX - nodeB.gridX);
-        int dstY = abs(nodeA.gridY - nodeB.gridY);
-
-        // 14 is the unit for diagonals and 10 is non-diagonals, this is a fancy math equation refer to notes on A* algorithm
-        if (dstX > dstY)
-        {
-            return 14 * dstY + 10 * (dstX - dstY);
-        }
-        return 14 * dstX + 10 * (dstY - dstX);
-    }
+    int GetDistance(Node nodeA, Node nodeB);
 
 
 };
--- LBDEngine/Source/Main/Game.cpp
+++ LBDEngine/Source/Main/Game.cpp
@@ -169,6 +169,7 @@ void Game::CreatePlayer()
 std::vector<Node> pathToGoal;
 Node* nextNode;
 GameObject* _enemy;
+float aiTime = GameTime::GetTotalTime();
 void Game::CreateEnemy()
 {
 	_enemy = CreateDynamicMeshObject("shape", "sphere", "stone", 3.0f, XMMatrixScaling(1.0f, 1.0f, 1.0f), XMMatrixRotationRollPitchYaw(0.0f, 0.0f, 0.0f), XMMatrixTranslation(0.0f, 0.5f, 10.0f), XMLoadFloat4x4(&MathHelper::CreateIdentity4x4()));
@@ -177,7 +178,15 @@ void Game::CreateEnemy()
 	MyGrid myGrid{};
 	auto behaviours = Game::GetBehavioursOfType<Collider>();
 	for (auto behaviour : behaviours) {
-		if (behaviour->GetGameObject() == _enemy || behaviour->GetGameObject() == _flag) continue;
+		if (behaviour->GetGameObject() == _enemy) {
+			myGrid.enemyRadiusX = behaviour->GetBoundingBox().Extents.x;
+			myGrid.enemyRadiusY = behaviour->GetBoundingBox().Extents.y;
+			myGrid.enemyRadiusZ = behaviour->GetBoundingBox().Extents.z;
+			continue;
+		}
+		if (behaviour->GetGameObject() == _flag) {
+			continue;
+		}
 		XMFLOAT3 location = behaviour->GetBoundingBox().Center;
 		myGrid.locations.push_back(Vector3{ location.x, location.y, location.z });
 		XMFLOAT3 size = behaviour->GetBoundingBox().Extents;
@@ -200,31 +209,42 @@ void Game::CreateEnemy()
 	}*/
 
 }
-int i = 0;
 void Game::UpdateAI() {
 	float range = 1.0;
 	float directionalForce = 0.000002f;
 	float upwardForce = 0.0000148f;
 
-
-	if (Vector3(_enemy->GetTranslation()._41, _enemy->GetTranslation()._42, _enemy->GetTranslation()._43)
-	< Vector3(nextNode->worldPosition.x + range, nextNode->worldPosition.y + range, nextNode->worldPosition.z + range) &&
-	Vector3(nextNode->worldPosition.x - range, nextNode->worldPosition.y - range, nextNode->worldPosition.z - range)
-	< Vector3(_enemy->GetTranslation()._41, _enemy->GetTranslation()._42, _enemy->GetTranslation()._43)) {
-		
-		nextNode++;
+	if (GameTime::GetTotalTime() - aiTime > 0.05) {
+		aiTime = GameTime::GetTotalTime();
 	}
 	else {
-		Vector3 direction{ nextNode->worldPosition.x - _enemy->GetTranslation()._41,
-			nextNode->worldPosition.y - _enemy->GetTranslation()._42 , nextNode->worldPosition.z - _enemy->GetTranslation()._43 };
-		XMMATRIX position{ 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, nextNode->worldPosition.x, 0.5f, nextNode->worldPosition.z, 1.0f };
-		i++;
-		if (i > pathToGoal.size()) 
-			return;
-		_enemy->SetTranslation(position);
-		_enemy->GetBehaviour<Mesh>()->SetDirtyFrames(3);
-		//_enemy->GetBehaviour<PhysicsBody>()->AddForce(XMFLOAT3(direction.x * directionalForce, upwardForce, direction.z * directionalForce));
+		return;
 	}
+
+	//if (Vector3(_enemy->GetTranslation()._41, _enemy->GetTranslation()._42, _enemy->GetTranslation()._43)
+	//< Vector3(nextNode->worldPosition.x + range, nextNode->worldPosition.y + range, nextNode->worldPosition.z + range) &&
+	//Vector3(nextNode->worldPosition.x - range, nextNode->worldPosition.y - range, nextNode->worldPosition.z - range)
+	//< Vector3(_enemy->GetTranslation()._41, _enemy->GetTranslation()._42, _enemy->GetTranslation()._43)) {
+	//	
+	//	nextNode++;
+	//}
+	//else {
+	//	Vector3 direction{ nextNode->worldPosition.x - _enemy->GetTranslation()._41,
+	//		nextNode->worldPosition.y - _enemy->GetTranslation()._42 , nextNode->worldPosition.z - _enemy->GetTranslation()._43 };
+	//	Node* endNode = &pathToGoal[pathToGoal.size() - 1];
+	//	if (nextNode > endNode)
+	//		return;
+	//	_enemy->GetBehaviour<PhysicsBody>()->AddForce(XMFLOAT3(direction.x * directionalForce, upwardForce, direction.z * directionalForce));
+	//}
+
+	XMMATRIX position{ 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, nextNode->worldPosition.x, 0.5f, nextNode->worldPosition.z, 1.0f };
+	Node* endNode = &pathToGoal[pathToGoal.size() - 1];
+	if (nextNode > endNode)
+		return;
+	_enemy->SetTranslation(position);
+	_enemy->GetBehaviour<Mesh>()->SetDirtyFrames(3);
+	nextNode++;
+
 }
 
 void Game::UpdateObjectCBs()
